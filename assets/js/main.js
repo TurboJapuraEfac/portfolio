57ad3d06ac3d0499196f593042db87dbd1bc88bf
Buddhika Weerasinghe <54759523+TurboJapuraEfac@users.noreply.github.com>
2020-12-25 12:58:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,6 @@ function topFunction() {
 let header = $(`
 <nav class="fixed-top black">
 <div class="nav-wrapper">
-    <a href="#!" class="brand-logo white-text"><i>Bu</i></a>
     <a href="#" data-target="mobile-demo" class="white-text sidenav-trigger"><i
             class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
